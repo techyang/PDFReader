728c43b3e15d245106fb893a1c01c9e449fdd153
techyang <tecyang@gmail.com>
2026-07-12 13:35:09 +0800

    feat: support opening a PDF via command-line argument

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 )
 
 func main() {
-	if _, err := ui.Run(""); err != nil {
+	var initialFile string
+	if len(os.Args) > 1 {
+		if info, err := os.Stat(os.Args[1]); err == nil && !info.IsDir() {
+			initialFile = os.Args[1]
+		}
+	}
+
+	if _, err := ui.Run(initialFile); err != nil {
 		fmt.Fprintln(os.Stderr, "pdfreader:", err)
 		os.Exit(1)
 	}
